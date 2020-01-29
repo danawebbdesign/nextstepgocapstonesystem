5ce96b94e259a076bc5636eb2c24b505b18c9bea
danawebbdesign <60206935+danawebbdesign@users.noreply.github.com>
2020-01-29 09:40:46 -0500

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,114 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	"use strict";
+
+	
+//Login Card ----------------------------------
+$("#card").flip({
+	trigger: "manual"
+});
+
+$("#flip-btn").click(function(){
+	$("#card").flip(true);
+});
+	
+$("#unflip-btn").click(function(){
+	$("#card").flip(false);
+});
+
+	
+	
+//Collapsible Sidebar -------------------------
+
+	
+	$("#sidebar-btn").click(function() {     
+    
+	//COLLAPSED VERSION
+	if($('#collapse-it').hasClass("sidebar-expanded")){
+        $( ".nav-item-side" ).animate({
+			borderLeft: "0px",
+			marginTop: "10px",
+			paddingBottom: "10px"
+		});
+		$( ".sidebar-active" ).css( "borderLeft", "0px" );
+		$( ".sidebar-active" ).css( "backgroundColor", "#434c55" );
+		$( ".sidebar-active" ).css( "color", "#83bf4a" );
+		$( ".nav-link-side" ).animate({
+			paddingTop: "8px",
+			marginLeft: "-5px",
+			paddingBottom: "15px"
+		});
+		$( "#postings" ).animate({
+			marginLeft: "10%",
+		});
+		$( ".nav-txt" ).fadeOut("100");
+		$("#collapse-it").addClass("collapsed-sidebar");
+		$("#collapse-it").removeClass("sidebar-expanded");
+		$( "#collapse-it" ).addClass( "col-1");
+		$('#collapse-it').removeClass("col-2");
+		$( "#mainbody" ).addClass( "col-11");
+		$('#mainbody').removeClass("col-10");
+		$( "#maincalbody" ).addClass( "col-11");
+		$('#maincalbody').removeClass("col-10");
+		$( "#sidebar-btn" ).animate({
+   			marginTop: "20px",
+			marginLeft: "45px"
+    	}, 500 );	
+		$( ".profile-img" ).animate({
+   			width: "80%",
+			margin: "0px",
+			marginLeft: "-10px",
+			marginTop: "10px",
+    	}, 800 );
+		$( "#signout-btn" ).css("textAlign", "center" ).css("paddingRight", "5px" );
+		$( ".btn-sign-out" ).css("marginLeft", "-10px" );
+	}
+	//EXPANDED VERSION
+    else {
+		$( ".nav-link-side" ).animate({
+			height: "40px",
+			paddingTop: "8px",
+		});
+        $( ".nav-txt" ).fadeIn("100");
+		$("#collapse-it").removeClass("collapsed-sidebar");
+		$("#collapse-it").addClass("sidebar-expanded"); 
+		$( "#collapse-it" ).addClass( "col-2");
+		$('#collapse-it').removeClass("col-1");
+		$( "#mainbody" ).addClass( "col-10");
+		$('#mainbody').removeClass("col-11");
+		$("#sidebar-btn").css({'transform': 'rotate(0deg)'});
+		$( "#sidebar-btn" ).animate({
+   			marginTop: "20px",
+			marginLeft: "150px"
+    	}, 500 );	
+		$( ".profile-img" ).animate({
+   			width: "80%",
+			marginLeft: "10px",
+    	}, 800 );
+		$( ".sidebar-active" ).css( "borderLeft", "solid thick #83bf4a" );
+		$( ".sidebar-active" ).css( "backgroundColor", "#525d69" );
+		$( "#postings" ).animate({
+			marginLeft: "20%",
+		});
+	}   
+	});
+	
+	var value = 0;
+		$("#sidebar-btn").rotate({
+			bind:
+			{
+				click: function(){
+					value +=180;
+					$(this).rotate({ animateTo:value});
+				}
+			}
+		});
+	
+
+//CALENDAR
+	
+	$("#todayDate").html(Date);
+	
+	
+});
---
+++ js/formtoggler.js
@@ -0,0 +1,113 @@
+// JavaScript Document
+
+$(document).ready(function(){
+	"use strict";
+	
+
+	//$("#jobOpp").click(function(){
+	  		//$("#postingForm").empty();
+			//alert("Hello! You clicked me!");
+	 // });
+	
+	$('input:radio[name=options]').change(function() {
+        if (this.value === 'jobOpp') {
+			$("#postingFormJob").css("display", "block");
+			$("#postingFormLearn").css("display", "none");
+			$("#postingFormScholar").css("display", "none");
+        }
+        else if (this.value === 'learnOpp') {
+			$("#postingFormJob").css("display", "none");
+			$("#postingFormLearn").css("display", "block");
+			$("#postingFormScholar").css("display", "none");
+        }
+		else if (this.value === 'scholarship') {
+            $("#postingFormJob").css("display", "none");            
+			$("#postingFormLearn").css("display", "none");
+			$("#postingFormScholar").css("display", "block");
+        }
+		
+    });
+	
+	//DELETE POST
+	//$(".deleteBtn").click(function(){
+	//	$(this).addClass("toBeDeleted");
+	//});	
+	
+	//DELETE POST
+	$(document).on('click', ".deleteBtn", function () {
+		$(this).addClass("toBeDeleted");
+	});
+		
+	$(".confirmDelete").click(function(){
+		$('#deleteModal').modal('hide'); 
+		//hide the modal
+		$('body').removeClass('modal-open'); 
+		//modal-open class is added on body so it has to be removed
+		$('.modal-backdrop').remove();
+		//need to remove div with modal-backdrop class
+		$('.toBeDeleted').closest('.card').remove();
+		//$(this).parents('.card').remove();
+		//alert("Hello! You clicked me!");
+	});
+	//Cancel Delete
+	$(".cancelDelete").click(function(){
+		$('#deleteModal').modal('hide'); 
+		//hide the modal
+		$('body').removeClass('modal-open'); 
+		//modal-open class is added on body so it has to be removed
+		$('.modal-backdrop').remove();
+		//need to remove div with modal-backdrop class
+		$('.toBeDeleted').removeClass('toBeDeleted');
+		//$(this).parents('.card').remove();
+		//alert("Hello! You clicked me!");
+	});
+	
+	
+	//NEW POST
+	$(".addPost").click(function(){
+		$('#newPostModal').modal('hide'); 
+		//hide the modal
+		$('body').removeClass('modal-open'); 
+		//modal-open class is added on body so it has to be removed
+		$('.modal-backdrop').remove();
+		//need to remove div with modal-backdrop class
+		
+		var jobCard=$('.jobCardTemplate').clone().addClass('jobCard');
+    	$('.posting-row').prepend(jobCard);
+		$('.jobCard').css('display', 'block');		
+		$('.jobCard').removeClass('jobCardTemplate');
+	});
+	
+	
+	//UNPUBLISH AND PUBLISH
+	$(document).on('click', ".publish-toggle", function () {
+		
+		//IF  UNPUBLISHED, PUBLISH
+		if ($(this).closest(".card").hasClass("unpublished")){
+			$(this).closest(".card").removeClass("unpublished");
+			$(this).closest(".unpublished-text").css("display", "none");
+			$(this).closest(".published-text").css("display", "block");
+			var currentdate = new Date(); 
+    		var datetime = "Published: " + currentdate.getDate() + "/" + (currentdate.getMonth()+1)  + "/" + currentdate.getFullYear() + " at "  + currentdate.getHours() + ":"  + currentdate.getMinutes();
+			$(this).closest(".published-text").html(datetime);
+		}
+		//IF  PUBLISHED, THEN UNPUBLISH
+		else{
+			$(this).closest(".unpublished-text").css("display", "block");
+			$(this).closest(".published-text").css("display", "none");
+			$(this).closest(".card").addClass("unpublished");
+		}
+});
+	
+	
+
+	
+  $("#examplesearch").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $(".job-post-title").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1);
+    });
+  });
+
+
+});
